ed20b260b40a04dcedd0665489478d9087edbd4f
brendan <bvincelli18@gmail.com>
2017-01-26 16:33:53 -0500

    added the checkInput() method

--- app/src/main/java/com/example/brendan/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/brendan/tipcalculator/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +13,10 @@ import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.CompoundButton;
+import android.widget.CheckBox;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +25,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox2);
+
+
+        if (checkBox.isChecked()){
+
+
+
+        }
     }
+
+
+
+    //TODO: make check() method
+    /*
+    This method checks if the bill amount entered is a valid number
+     */
+public boolean checkInput(View view){
+
+    // set to true for now , adjust when writing the method
+    boolean isValid ;
+
+    // find the bill input field
+    TextView editText = (TextView) findViewById(R.id.editText2);
+
+    //parse input as string
+    String input = editText.getText().toString();
+
+    float number;
+
+    try {
+        // if parsed as float
+        number = Float.parseFloat(input);
+        isValid= true;
+        return isValid;
+
+
+        // input was not valid
+    } catch (NumberFormatException e) {
+
+        Toast.makeText(this, "Not a Valid Input!", Toast.LENGTH_LONG).show();
+        isValid = false;
+        return isValid;
+
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
 }
 
 //TODO: spinner has been populated with entries
@@ -32,7 +93,7 @@ need to code the ogic for the app now. Below if the order to follow
 
  - checks : takes the entered value for the bill amount , checks if HST is enabled (use boolean value), checks what the tip percent is from the spinner( uses it to calculate total)
 
- - calls : it should pass these vaues to another method that displays the information (make it as modular as possible)
+ - calls : it should pass these values to another method that displays the information (make it as modular as possible)
 
  -display(): display the results of the calculations
 
